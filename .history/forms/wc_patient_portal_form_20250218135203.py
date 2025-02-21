f6b9f376cef00dc196875eb70b511ef9b61d0fcc
DavidG626 <davidguticodes@gmail.com>
2025-02-21 14:12:22 -0800

    8

---
+++ .history/forms/wc_patient_portal_form_20250218135203.py
@@ -0,0 +1,66 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class wc_PatientRegistrationForm(FlaskForm):
+    provider_first_name = StringField('Provider First Name', validators=[DataRequired()])
+    provider_last_name = StringField('Provider Last Name', validators=[DataRequired()])
+    patient_first_name = StringField('Patient First Name', validators=[DataRequired()])
+    patient_last_name = StringField('Patient Last Name', validators=[DataRequired()])
+    
+    patient_address = StringField('Address', validators=[DataRequired()])
+    patient_city = StringField('City', validators=[DataRequired()])
+    patient_state = StringField('State', validators=[DataRequired()])
+    patient_zip_code = StringField('ZIP Code', validators=[DataRequired()])
+    
+    # DOB fields
+    patient_dob_month = StringField('Month', validators=[DataRequired()])
+    patient_dob_day = StringField('Day', validators=[DataRequired()])
+    patient_dob_year = StringField('Year', validators=[DataRequired()])
+    
+    # DOI fields
+    doi_month = StringField('Month', validators=[DataRequired()])
+    doi_day = StringField('Day', validators=[DataRequired()])
+    doi_year = StringField('Year', validators=[DataRequired()])
+    
+    # Medical Condition fields
+    medical_condition_1 = StringField('Medical Condition 1')
+    medical_condition_2 = StringField('Medical Condition 2')
+    medical_condition_3 = StringField('Medical Condition 3')
+    medical_condition_4 = StringField('Medical Condition 4')
+    medical_condition_5 = StringField('Medical Condition 5')
+    medical_condition_6 = StringField('Medical Condition 6')
+    
+    claim_number = StringField('Claim Number', validators=[DataRequired()])
+    
+    # Employer fields
+    employer = StringField('Employer Name', validators=[DataRequired()])
+    employer_address = StringField('Employer Address', validators=[DataRequired()])
+    employer_city = StringField('Employer City', validators=[DataRequired()])
+    employer_state = StringField('Employer State', validators=[DataRequired()])
+    employer_zip_code = StringField('Employer ZIP Code', validators=[DataRequired()])
+    employer_phone = StringField('Employer Phone', validators=[DataRequired()])
+    employer_fax = StringField('Employer Fax')
+    
+    # Claims Admin fields
+    admin_name = StringField('Administrator Name', validators=[DataRequired()])
+    adjustor = StringField('Adjustor Name', validators=[DataRequired()])
+    adjustor_address = StringField('Adjustor Address', validators=[DataRequired()])
+    adjustor_city = StringField('Adjustor City', validators=[DataRequired()])
+    adjustor_state = StringField('Adjustor State', validators=[DataRequired()])
+    adjustor_zip_code = StringField('Adjustor ZIP Code', validators=[DataRequired()])
+    adjustor_phone = StringField('Adjustor Phone', validators=[DataRequired()])
+    adjustor_fax = StringField('Adjustor Fax')
+    adjustor_email = StringField('Adjustor Email')
+    
+    # Lawyer fields
+    lawyer_name = StringField('Lawyer Name')
+    lawyer_address = StringField('Lawyer Address')
+    lawyer_city = StringField('Lawyer City')
+    lawyer_state = StringField('Lawyer State')
+    lawyer_zip_code = StringField('Lawyer ZIP Code')
+    lawyer_phone = StringField('Lawyer Phone')
+    lawyer_fax = StringField('Lawyer Fax')
+    lawyer_email = StringField('Lawyer Email')
+    
+    submit = SubmitField('Register Patient')
---
+++ .history/models/wc_patient_models_20250218133718.py
@@ -0,0 +1,87 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from extensions import provider_info_db as db
+from .patient_medical_hx_models import PastMedicalHistory
+from .icd10_models import ICD10Code
+
+class Patient(db.Model):
+   __tablename__ = 'patient'
+   
+   id = db.Column(db.Integer, primary_key=True)
+   patient_first_name = db.Column(db.String(20), nullable=False)  
+   patient_last_name = db.Column(db.String(20), nullable=False)
+   patient_sex = db. Column(db)
+   patient_address = db.Column(db.String(200), nullable=False)
+   patient_city = db.Column(db.String(50), nullable=False)
+   patient_state = db.Column(db.String(20), nullable=False) 
+   patient_zip_code = db.Column(db.String(10), nullable=False)
+   patient_date_of_birth = db.Column(db.Date, nullable=False)
+   patient_date_of_injury = db.Column(db.Date, nullable=False)
+   patient_claim_number = db.Column(db.String(50), nullable=False)
+   diagnoses = db.relationship('ICD10Code', secondary='patient_diagnoses', 
+                              backref=db.backref('patients', lazy='dynamic'))
+   
+   # Provider relationship
+   provider_first_name = db.Column(db.String(20), nullable=False)
+   provider_last_name = db.Column(db.String(20), nullable=False)
+   
+   # Relationships to other tables
+   employer = db.relationship('Employer', backref='patient', uselist=False, cascade='all, delete-orphan')
+   claims_admin = db.relationship('ClaimsAdmin', backref='patient', uselist=False, cascade='all, delete-orphan')
+   lawyer = db.relationship('Lawyer', backref='patient', uselist=False, cascade='all, delete-orphan')
+   medical_history = db.relationship('PastMedicalHistory', backref='patient', uselist=False, cascade='all, delete-orphan')
+   lab_results = db.relationship('LabResult', backref='patient', cascade='all, delete-orphan')
+   imaging_results = db.relationship('ImagingResult', backref='patient', cascade='all, delete-orphan')
+   operative_reports = db.relationship('OperativeReport', backref='patient', cascade='all, delete-orphan')
+   misc_results = db.relationship('Misc', backref='patient', cascade='all, delete-orphan')
+   authorizations = db.relationship('Auth', backref='patient', cascade='all, delete-orphan')
+
+
+
+class Employer(db.Model):
+   __tablename__ = 'employer'
+   
+   id = db.Column(db.Integer, primary_key=True)
+   patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+   employer_name = db.Column(db.String(100), nullable=False)
+   employer_address = db.Column(db.String(200), nullable=False)
+   employer_city = db.Column(db.String(50), nullable=False)
+   employer_state = db.Column(db.String(20), nullable=False)
+   employer_zip_code = db.Column(db.String(10), nullable=False)
+   employer_phone = db.Column(db.String(20), nullable=False)
+   employer_fax = db.Column(db.String(20))
+
+class ClaimsAdmin(db.Model):
+   __tablename__ = 'claims_admin'
+   
+   id = db.Column(db.Integer, primary_key=True)
+   patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+   admin_name = db.Column(db.String(100), nullable=False)
+   adjustor = db.Column(db.String(100), nullable=False)
+   adjustor_address = db.Column(db.String(200), nullable=False)
+   adjustor_city = db.Column(db.String(50), nullable=False)
+   adjustor_state = db.Column(db.String(20), nullable=False)
+   adjustor_zip_code = db.Column(db.String(10), nullable=False)
+   adjustor_phone = db.Column(db.String(20), nullable=False)
+   adjustor_fax = db.Column(db.String(20))
+   adjustor_email = db.Column(db.String(120))
+
+class Lawyer(db.Model):
+   __tablename__ = 'lawyer'
+   
+   id = db.Column(db.Integer, primary_key=True)
+   patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+   lawyer_name = db.Column(db.String(100))
+   lawyer_address = db.Column(db.String(200))
+   lawyer_city = db.Column(db.String(50))
+   lawyer_state = db.Column(db.String(20))
+   lawyer_zip_code = db.Column(db.String(10))
+   lawyer_phone = db.Column(db.String(20))
+   lawyer_fax = db.Column(db.String(20))
+   lawyer_email = db.Column(db.String(120))
+
+class PatientDiagnosis(db.Model):
+    __tablename__ = 'patient_diagnoses'
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+    icd10_id = db.Column(db.Integer, db.ForeignKey('icd10_codes.id'), nullable=False)
